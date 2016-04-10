467588401ceb65ecfcbb73e7b3264aaf34c6ee9f
astatsegre <astatsegre@gmail.com>
2016-04-10 12:08:47 +0300

    11

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
 var menuIcon = document.querySelector('.icon-menu');
+var btnLeft = document.querySelector('.btn-left');
+var btnRight = document.querySelector('.btn-right');
+var galleryDiv = document.querySelectorAll('.desctop-gallery-container .photo-container div');
+var photoContainer = document.querySelector('.photo-container');
+var marginLeft = 0;
+var galleryLength = (galleryDiv.length - 3) * 327;
 
 menuIcon.addEventListener('click', function(){
     var menu = document.querySelector('.main-nav-list');
     
     menu.classList.toggle('unhide');
 })
+
+btnRight.addEventListener('click', function() {
+    
+    if (marginLeft > -galleryLength) {
+        photoContainer.style.marginLeft = marginLeft - 327 + "px";
+        marginLeft = marginLeft - 327;
+    }
+})
+
+btnLeft.addEventListener('click', function() {
+    if (marginLeft < 0) {
+        photoContainer.style.marginLeft = marginLeft + 327 + "px";
+        marginLeft = marginLeft + 327;
+    }
+})
+
+
+
+
+
+
+
